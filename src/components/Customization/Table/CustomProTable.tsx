30b79e37d9724c3f6a6145b61de6faa507b3dd0a
tju <tony.ju@me.com>
2025-11-01 09:43:38 +0800

    feat: index栏位位置与国际化

--- src/components/Customization/Table/CustomProTable.tsx
+++ src/components/Customization/Table/CustomProTable.tsx
@@ -1,19 +1,22 @@
 import { type ProColumns, ProTable } from '@ant-design/pro-components';
+import { useIntl } from '@umijs/max';
 import React, { useMemo } from 'react';
 import type { CustomProTableProps } from '@/components/Common/Pages/List/typing'; // 导入你的 Props
 
 // 序号列的定义
-const indexColumn: ProColumns<any> = {
-  title: '序号',
+const indexColumn = (intl: any): ProColumns<any> => ({
+  title: intl.formatMessage({ id: 'common.index' }),
   dataIndex: 'index',
   valueType: 'indexBorder',
   width: 48,
   hideInSearch: true,
-};
+  align: 'center',
+});
 
 export function CustomProTable<T extends Record<string, any>>(
   props: CustomProTableProps<T>,
 ) {
+  const intl = useIntl();
   // 1. 分离出你的自定义 Prop 和 ProTable 的原生 Props
   const { showIndexColumn = false, ...restProps } = props;
 
@@ -21,10 +24,10 @@ export function CustomProTable<T extends Record<string, any>>(
   const finalColumns = useMemo(() => {
     if (showIndexColumn) {
       // 确保 props.columns 是一个数组
-      return [indexColumn, ...(props.columns || [])];
+      return [indexColumn(intl), ...(props.columns || [])];
     }
     return props.columns;
-  }, [props.columns, showIndexColumn]);
+  }, [props.columns, showIndexColumn, intl]);
 
   // 3. 渲染 ProTable，传入计算后的 props
   return (
